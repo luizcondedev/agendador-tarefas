87ac202308e8be79acf31f407707a57652d2e2d8
Luiz Conde <luizconde.deev@gmail.com>
2026-07-23 15:37:01 -0300

    correção de código

--- src/main/java/com/luizconde/agendadortarefas/infrastructure/client/UsuarioClient.java
+++ src/main/java/com/luizconde/agendadortarefas/infrastructure/client/UsuarioClient.java
@@ -2,7 +2,6 @@ package com.luizconde.agendadortarefas.infrastructure.client;
 
 import com.luizconde.agendadortarefas.business.dto.UsuarioDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "usuarios", url = "${usuario.url}")
